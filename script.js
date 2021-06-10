015e254cf749d25f64a90bcf9339135b999807b8
denis-ttk-1975 <denis.ttk@gmail.com>
2021-06-10 16:59:18 +0300

    before debugging

--- script.js
+++ script.js
@@ -14,3 +14,76 @@ P.S. Функции вызывать не обязательно*/
 'use strict';
 
 // Код возьмите из предыдущего домашнего задания
+
+alert('Practice Task #3');
+
+let numberOfFilms = 0;
+
+do {numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?????!!!!!!!!!!');}
+while (numberOfFilms<=0);
+
+alert(numberOfFilms);
+
+let personalMovieDB = {count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false,
+};
+console.log(personalMovieDB);
+
+let filmName = '';
+let filmScore = '';
+
+for (let i=0; i<=1; i++)  {
+
+	do {filmName = prompt('Один из последних просмотренных фильмов?');} 
+	while ( filmName == '' || filmName == 0 || filmName == null || filmName.length > 50);
+	do {filmScore = prompt('На сколько оцените его?');}
+	while ( filmScore == '' || filmScore == 0 || filmScore == null || +filmScore > 100);
+	
+	personalMovieDB.movies[filmName] = filmScore;
+}
+
+console.log(personalMovieDB);
+
+let i1 = 0;
+
+while (i1<2)  {
+
+	do {filmName = prompt('Один из последних просмотренных фильмов?');} 
+	while ( filmName == '' || filmName == 0 || filmName == null || filmName.length > 50);
+	do {filmScore = prompt('На сколько оцените его?');}
+	while ( filmScore == '' || filmScore == 0 || filmScore == null || +filmScore > 100);
+	
+	personalMovieDB.movies[filmName] = filmScore;
+	i1++;
+}
+
+console.log(personalMovieDB);
+
+if (personalMovieDB.count < 10) {
+	alert ('Просмотрено довольно мало фильмов');
+} else if ((personalMovieDB.count >= 10) || (personalMovieDB.count < 30)) {
+	alert ('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+	alert ('Вы киноман');
+} else {alert ('Произошла ошибка');}
+
+function showMyDB () {
+	if (!personalMovieDB.privat) {
+		console.log(personalMovieDB.privat, personalMovieDB);
+	}
+}
+
+showMyDB();
+
+function writeYourGenres () {
+	for (let i = 0; i <= 2; i++) {
+		let a = prompt(`Ваш любимый жанр под номером ${i}`);
+		personalMovieDB.genres[i] = a;
+	}
+}
+
+writeYourGenres();
+showMyDB();
